fb615b6ee6bde44022f80d137ea002cc2025ed3d
Robert <F8VPE@users.noreply.github.com>
2026-05-25 22:05:00 +0200

    Create connectWiFi.ino

---
+++ connectWiFi.ino
@@ -0,0 +1,19 @@
+// =========================== connectWiFi ==============================================================
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);  // Set WiFi mode to station (client)
+  //  WiFi.config( ip, gateway, subnet );
+#ifdef DEBUG
+  Serial.print(ssid);  // to know which
+  Serial.print(" ");
+#endif
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+#ifdef DEBUG
+    Serial.print(".");
+#endif
+  }
+#ifdef DEBUG
+  Serial.println(" ");
+#endif
+}
